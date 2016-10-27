9e270fce48ce442b1da28da2ccb9390676f50f08
Allison Padget <allison@macbook-pro.gateway.pace.com>
2016-10-26 20:53:14 -0600

    adding widgets

--- js/dailytraffic.js
+++ js/dailytraffic.js
@@ -1,1 +1,24 @@
 /** DAILY TRAFFIC BAR CHART WIDGET **/
+
+var dailyTraffic = document.getElementById("daily-traffic");
+
+var dailyTraffic = new Chart(dailyTraffic, {
+    type: 'bar',
+    data: {
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
+        datasets: [{
+            label: '',
+            data: [75, 100, 175, 75, 225, 200, 100],
+            backgroundColor: [
+                '#E2E3F6'
+            ],
+            borderColor: [
+                '#7377BF'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+
+    }
+});
--- js/webtraffic.js
+++ js/webtraffic.js
@@ -1,1 +1,28 @@
-/** WEB TRAFFIC WIDGET **/
+/** WEB TRAFFIC WIDGET **/ 
+
+/* Display number of visits along the y axis and
+date/time on the x axis. You will need to make up this data -- you can see the
+mockup for sample data.*/
+
+var webTraffic = document.getElementById("web-traffic");
+
+var webTraffic = new Chart(webTraffic, {
+    type: 'line',
+    data: {
+        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+        datasets: [{
+            label: '',
+            data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2200, 2000],
+            backgroundColor: [
+                '#E2E3F6'
+            ],
+            borderColor: [
+                '#7377BF'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+
+    }
+});
